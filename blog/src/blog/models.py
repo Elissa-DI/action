69cafc9c29e5119771441e0ab6e2c17ac952f47b
Elissa-DI <elissadusabe@gmail.com>
2024-10-17 11:46:31 +0200

    started blog

---
+++ blog/src/blog/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class CreateUpdateModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+        
+STATUS_CHOICES = (
+    ('Draft', 'Draft'),
+    ("Publish", "Publish"),
+    ("Archive", "Archive")
+)
+class Post(CreateUpdateModel):
+    title = models.CharField(max_length=120)
+    slug = models.CharField(max_length=120, unique=True)
+    content = models.TextField()
+    status = models.CharField(max_length=10, default='Draft', choices=STATUS_CHOICES)
+    hero_image = models.ImageField(upload_to='post', default="post/sample.jpg")
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    
+class Comment(CreateUpdateModel):
+    text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    is_comment_approved = models.BooleanField(default=False)
+
